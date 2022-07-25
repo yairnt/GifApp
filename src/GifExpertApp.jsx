5421dbd99daad2e127c1d2e51779e451449eaa4c
yairnt <yair.nt@hotmail.com>
2022-07-25 06:10:21 -0300

    GifApp, first commit

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -14,8 +14,9 @@ const onAddCategory = (newCategory) => {
     <>
       <h1>GifExpertApp</h1>
       
-      <AddCategory /*setCategories={setCategories}*/
-        onNewCategory={ (event) => onAddCategory(event)} />
+      <AddCategory 
+        onNewCategory={ (event) => onAddCategory(event)}
+      />
       <ol>
         { categories.map (category => {
           return <li key={ category }>{category}</li>
