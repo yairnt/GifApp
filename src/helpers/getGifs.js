8e44cbc828309230e876e3d3cf339c7a2829313a
yairnt <yair.nt@hotmail.com>
2022-07-26 15:16:11 -0300

    minor fix

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(category) => {
 
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=nrHvnUxtQLAcnJNdSZ4BFVx1j8sR5x3c&q=${category}&limit=8`
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=nrHvnUxtQLAcnJNdSZ4BFVx1j8sR5x3c&q=${category}&limit=8`
   const resp = await fetch( url )
   const {data} = await resp.json();
 
